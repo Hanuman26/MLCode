e3276472fc0660e9490c027d087ab6ec82c03f09
Manish Pande <manishpande1520@gmail.com>
2015-12-05 13:05:15 +0530

    gradient descent on linear regression

--- gradient-descent/gradient-descent-linear-regression.py
+++ gradient-descent/gradient-descent-linear-regression.py
@@ -1,17 +1,20 @@
 #This is a simple implementation of gradient descent for linera regression on a simle two dimensional data stored in data.csv
 from numpy import *
-
+from matplotlib import pyplot as plt
 def run():
     points = genfromtxt("data.csv", delimiter=",")
     learning_rate = 0.0001
 # y = mx+b
     initial_b = 0
     initial_m = 0
-    num_iterations = 1500
+    num_iterations = 200
     print "Starting gradient descent at b = {0}, m = {1}, error = {2}".format(initial_b, initial_m, compute_error(initial_b, initial_m, points))
     print "********************************************************"
     [b, m] = gradient_descent(points, initial_b, initial_m, learning_rate, num_iterations)
     print "After {0} iterations b = {1}, m = {2}, error = {3}".format(num_iterations, b, m, compute_error(b, m, points))
+    plt.plot(b,m)
+    plt.show()
+	
 
 #function for calculating total error for candidate line 'y' value
 def compute_error(b, m, points):
