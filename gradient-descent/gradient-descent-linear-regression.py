267c0bf3f5b80787a2f50a87be669c4e127e63b6
Manish Pande <manishpande1520@gmail.com>
2015-12-05 13:10:00 +0530

    matplotlib figure

--- gradient-descent/gradient-descent-linear-regression.py
+++ gradient-descent/gradient-descent-linear-regression.py
@@ -1,18 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
 #This is a simple implementation of gradient descent for linera regression on a simle two dimensional data stored in data.csv
 from numpy import *
 from matplotlib import pyplot as plt
 def run():
-    points = genfromtxt("data.csv", delimiter=",")
+    points = genfromtxt("/home/mirk/ML/mycode/gradient-descent/data.csv", delimiter=",")
     learning_rate = 0.0001
 # y = mx+b
     initial_b = 0
     initial_m = 0
-    num_iterations = 200
+    num_iterations = 2000
     print "Starting gradient descent at b = {0}, m = {1}, error = {2}".format(initial_b, initial_m, compute_error(initial_b, initial_m, points))
     print "********************************************************"
     [b, m] = gradient_descent(points, initial_b, initial_m, learning_rate, num_iterations)
     print "After {0} iterations b = {1}, m = {2}, error = {3}".format(num_iterations, b, m, compute_error(b, m, points))
-    plt.plot(b,m)
+    x = points[:,0]
+    y = points[:,1]    
+    plt.scatter(x,y)
+    plt.plot(x,(m * x) + b)
     plt.show()
 	
 
@@ -43,6 +52,7 @@ def step_gradient(b_current, m_current, points, learning_rate):
 def gradient_descent(points, starting_b, starting_m, learning_rate, num_iterations):
     b = starting_b
     m = starting_m
+    image = []
     for i in range(num_iterations):
         b, m = step_gradient(b, m, array(points), learning_rate)
     return [b, m] 
